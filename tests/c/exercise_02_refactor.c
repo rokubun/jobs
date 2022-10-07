2a5a2cff2cc4f25d16564a860f97369f212a9a62
Miquel Garcia <miquel.garcia@rokubun.cat>
2022-10-07 14:17:20 +0200

    refactor: move quiz to Google forms

---
+++ tests/c/exercise_02_refactor.c
@@ -0,0 +1,7 @@
+/* The following code compiles and works as expected. However:
+
+    - How would you refactor the code to shrink it and make it more modular?
+
+    Hint: use dependency injection.
+*/
+
